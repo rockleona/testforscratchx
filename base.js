1f46d491ef8ee4cf1132b469fbbd44e8c75301b6
Rockleona <rocklonjp@gmail.com>
2018-01-03 15:52:24 +0800

    basecode

---
+++ base.js
@@ -0,0 +1,24 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.cacu = function(number){
+        return number**2
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ','square of %n','cacu']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
